378fe1b58846144774c680a6be7c351c923e524a
nmasalkov <nikita.masalkov@pandasoft.com>
2020-12-02 00:37:52 +0300

    add navbar and title pic

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :dashboard, only: [:index]
 
-
+  root to: "dashboard#index"
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
