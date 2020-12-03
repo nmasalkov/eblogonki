be60cdd6985430e52cadbcdd454fa93bc430117f
nmasalkov <nikita.masalkov@pandasoft.com>
2020-12-03 23:17:23 +0300

    add front

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   def index
-
+    @participants = Participant.all
+    @checkpoints = Checkpoint.all
+    @count = Checkpoint.count
+    # @leaders = User.all.sort_by(:points)
   end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,9 @@
 class Participant < ApplicationRecord
   has_many :tries
   has_many :stakes
+
+  def position
+    rand(8)
+  end
+
 end
